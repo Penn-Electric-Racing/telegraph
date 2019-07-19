84b33005ef66d8629cd0178e986a65023c2b25b0
Daniel Pfrommer <dan.pfrommer@gmail.com>
2019-07-19 22:23:34 +0200

    Made basic card demo

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -1,27 +1,38 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <card title="Hi" subtitle="Hello"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import card from './components/card.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    card
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Poppins", sans-serif, "Noto Color Emoji";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
+
+body {
+  background-color: #1c1e2f;
+  color: #b1b1b5;
+  border: 0;
+  margin: 0;
+}
+
+h1 {
+  font-weight: 30;
+}
+
 </style>
---
+++ gui/src/components/card.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card">
+        <h1>{{ title }}</h1>
+        <h2>{{ subtitle }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: {
+    title: String,
+    subtitle: String
+  }
+}
+</script>
+
+<style scoped>
+
+.card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  word-wrap: break-word;
+  background-color: #222437;
+  background-clip: border-box;
+  border-radius: 0.25rem;
+  border: 1px solid #313452
+}
+
+</style>
