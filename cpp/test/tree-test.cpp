617923e4019785643d324825d9f8ceb1923affd3
Daniel Pfrommer <dan.pfrommer@gmail.com>
2019-11-09 12:16:08 -0500

    First testing working

--- cpp/test/tree-test.cpp
+++ cpp/test/tree-test.cpp
@@ -14,25 +14,10 @@
 
 using namespace telegraph;
 
-int main(int argc, char** argv) {
-    char* dir = std::getenv("BUILD_WORKSPACE_DIRECTORY");
-    std::string file = (dir ? std::string(dir) : ".") + "/cpp/main/example.conf";
-    hocon_parser parser;
-    json j = parser.parse_file(file);
 
+int main(int argc, char** argv) {
+    hocon_parser parser;
+    json j = parser.parse_file("cpp/test/example.conf");
     tree* t = tree::unpack(j["root"]);
     std::cout << *t << std::endl;
-
-    context* ctx = new context("live");
-    ctx->set_tree(t); // ctx now owns t and will free t on destruction
-
-    server srv;
-    srv.start("0.0.0.0:8000");
-
-    {
-        auto cl = srv.contexts.lock();
-        srv.contexts.add(ctx);
-    }
-
-    srv.join();
 }
