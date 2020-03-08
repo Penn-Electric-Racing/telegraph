b8a7de2eb35efffc753360e0ee91bdcf5922cb14
pfrommerd <dan.pfrommer@gmail.com>
2020-03-08 13:07:06 -0400

    working on uart support
    * fetching tree working, adapter in-progress
    * adapter is now compiling

--- cpp/lib/telegraph/common/adapter.cpp
+++ cpp/lib/telegraph/common/adapter.cpp
@@ -35,36 +35,35 @@ namespace telegraph {
     }
 
     adapter::adapter(io::io_context& ioc,
-            const std::function<void(interval, interval)>& change,
-            const std::function<void()> cancel)
+            const std::function<bool(io::yield_ctx&, interval, interval, interval)>& change,
+            const std::function<bool(io::yield_ctx&, interval)> cancel)
         : ioc_(ioc), change_sub_(change), cancel_sub_(cancel), 
           subscribed_(false), min_interval_(0), 
-          max_interval_(0) {}
-
+          max_interval_(0), 
+          running_op_(false), waiting_ops_(),
+          subs_(), last_val_() {}
 
     subscription_ptr
     adapter::subscribe(io::yield_ctx& yield, 
             interval min_interval, interval max_interval, interval timeout) {
+        sub* s = new sub(this, min_interval, max_interval);
+
+        // if we successfully changed the subscription
         if (change(yield, min_interval, max_interval, timeout)) {
-            sub* s = new sub(this, min_interval, max_interval);
             subs_.insert(s);
-            // TODO: Queue sending last_val to s
             return subscription_ptr(s);
         } else {
+            delete s;
             return subscription_ptr();
         }
     }
 
     bool
     adapter::change(io::yield_ctx& yield, interval new_min, interval new_max, interval timeout) {
-        io::deadline_timer timer{ioc_, boost::posix_time::milliseconds(timeout)};
-        if (current_op_ != nullptr) {
-            io::deadline_timer wait{ioc_, boost::posix_time::milliseconds(timeout)};
+        if (running_op_) {
+            io::deadline_timer wait{ioc_};
             waiting_ops_.push_back(&wait);
             wait.async_wait(yield.ctx); // wait for our turn
-            if (current_op_) { // turn never came!
-                return false;
-            }
         }
 
         // no operations in the queue!
@@ -76,7 +75,7 @@ namespace telegraph {
         }
 
         // first determine if we need to re-request
-        bool rerequest = false;
+        bool subscribe = false;
         if (subscribed_) {
             for (sub* s : subs_) {
                 new_min = std::min(s->get_min_interval(), new_min);
@@ -84,27 +83,26 @@ namespace telegraph {
             }
             if (new_min < min_interval_ || new_max < max_interval_ ||
                     (new_max > 0 && max_interval_ == 0)) {
-                min_interval_ = std::min(new_min, min_interval_);
-                max_interval_ = max_interval_ > 0 ? std::min(new_max, max_interval_) : new_max;
-                rerequest = true;
+                // we need to re-sub
+                new_min = std::min(new_min, min_interval_);
+                new_max = max_interval_ > 0 ? std::min(new_max, max_interval_) : new_max;
+                subscribe = true;
             }
         } else {
-            // not yet subscribed, always re-request
-            min_interval_ = new_min;
-            max_interval_ = new_max;
-            rerequest = true;
+            subscribe = true;
         }
 
-        if (rerequest) {
+        if (subscribe) {
             // make the request!
-            current_op_ = &timer;
-            op_success_ = false;
-            change_sub_(new_min, new_max);
-            timer.async_wait(yield.ctx);
+            running_op_ = true;
+            bool success = change_sub_(yield, new_min, new_max, timeout);
+            running_op_ = false;
+            if (success) {
+                subscribed_ = true;
+                min_interval_ = new_min;
+                max_interval_ = new_max;
+            }
 
-            subscribed_ = op_success_;
-
-            current_op_ = nullptr;
             // notify the next person
             // that they can make their request
             if (waiting_ops_.size() > 0) {
@@ -112,32 +110,27 @@ namespace telegraph {
                 waiting_ops_.pop_front(); // remove their timer
                 t->cancel(); // cancel their timer so they can go
             }
-        } 
-        if (!subscribed_) {
-            // if the request failed, we need
+            return success;
         }
-        return subscribed_;
+        return true;
     }
 
     bool
     adapter::cancel(io::yield_ctx& yield, sub* s, interval timeout) {
-        subs_.erase(s);
-        io::deadline_timer timer{ioc_, boost::posix_time::milliseconds(timeout)};
-        if (current_op_ != nullptr) {
-            io::deadline_timer wait{ioc_, boost::posix_time::milliseconds(timeout)};
+        if (running_op_) {
+            io::deadline_timer wait{ioc_};
             waiting_ops_.push_back(&wait);
             wait.async_wait(yield.ctx); // wait for our turn
-            if (current_op_) { // turn never came!
-                return false;
-            }
         }
 
+        subs_.erase(s);
+        delete s;
+
         if (subs_.size() == 0) {
-            current_op_ = &timer;
-            op_success_ = false;
-            cancel_sub_();
-            timer.async_wait(yield.ctx);
-            current_op_ = nullptr;
+            running_op_ = true;
+            bool success = cancel_sub_(yield, timeout);
+            running_op_ = false;
+
             // notify the next person
             // that they can make their request
             if (waiting_ops_.size() > 0) {
@@ -145,7 +138,7 @@ namespace telegraph {
                 waiting_ops_.pop_front(); // remove their timer
                 t->cancel(); // cancel their timer so they can go
             }
-            return op_success_;
+            return success;
         } else {
             return change(yield, std::numeric_limits<interval>::max(), 0, timeout);
         }
