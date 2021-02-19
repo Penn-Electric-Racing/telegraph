1ec8a245dc761bb248878d027734febf8bea40f3
Zohar <injapanthree@gmail.com>
2021-02-19 16:10:31 -0500

    started on settings page

--- gui/src/pages/SettingsPage.vue
+++ gui/src/pages/SettingsPage.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
-        Settings! (Nothing here yet)
-    </div>
+	<ScrollArea> <SettingsView /> </ScrollArea>
 </template>
+
+<script>
+import ScrollArea from "../components/ScrollArea.vue";
+import SettingsView from "../views/SettingsView.vue";
+
+export default {
+	name: "SettingsPage",
+	components: { ScrollArea, SettingsView },
+	props: {},
+};
+</script>
