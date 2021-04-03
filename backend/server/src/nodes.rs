9256cf691e42b7fe3a4e1a03d23c68a6bad2568a
Will Vauclain <willvauclain.dev@gmail.com>
2021-04-02 23:48:55 -0400

    Implement more of the functionality in nodes.rs

--- backend/server/src/nodes.rs
+++ backend/server/src/nodes.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use std::fmt;
+use std::ops::Index;
 
 use crate::errors::UnpackError;
 use crate::types::Type;
@@ -36,12 +38,24 @@ pub struct NodeInfo {
     // ///
     // /// We store this using a weak reference so that a node doesn't accidentally keep its parent
     // /// alive.
-    // /// TODO: can this be a reference with a lifetime? Or is this necessary?
+    // /// TODO: can this be a reference with a lifetime? Or is this even necessary? What do we use
+    // /// a node's parent for anyway?
     // // parent: Option<Weak<Node>>
 
     // TODO: How should we handle contexts?
     // /// The enclosing context
     // // ctx: Weak<Refcell<Context>>
+}
+
+impl NodeInfo {
+    pub fn new<S: AsRef<str>>(id: NodeID, name: S, pretty: S, desc: S) -> Self {
+        Self {
+            id,
+            name: String::from(name.as_ref()),
+            pretty: String::from(pretty.as_ref()),
+            desc: String::from(desc.as_ref()),
+        }
+    }
 }
 
 impl fmt::Display for NodeInfo {
@@ -64,18 +78,8 @@ impl Node {
         schema: S,
         version: i32,
     ) -> Self {
-        let info = NodeInfo {
-            id,
-            name: String::from(name.as_ref()),
-            pretty: String::from(pretty.as_ref()),
-            desc: String::from(desc.as_ref()),
-        };
-        Node::Group(Group {
-            info,
-            children,
-            schema: String::from(schema.as_ref()),
-            version,
-        })
+        let info = NodeInfo::new(id, name, pretty, desc);
+        Node::Group(Group::new(info, children, schema.as_ref().to_string(), version))
     }
 
     pub fn action<S: AsRef<str>>(
@@ -86,17 +90,8 @@ impl Node {
         arg_type: Type,
         ret_type: Type,
     ) -> Self {
-        let info = NodeInfo {
-            id,
-            name: String::from(name.as_ref()),
-            pretty: String::from(pretty.as_ref()),
-            desc: String::from(desc.as_ref()),
-        };
-        Node::Action(Action {
-            info,
-            arg_type,
-            ret_type,
-        })
+        let info = NodeInfo::new(id, name, pretty, desc);
+        Node::Action(Action::new(info, arg_type, ret_type))
     }
 
     pub fn variable<S: AsRef<str>>(
@@ -106,13 +101,8 @@ impl Node {
         desc: S,
         data_type: Type,
     ) -> Self {
-        let info = NodeInfo {
-            id,
-            name: String::from(name.as_ref()),
-            pretty: String::from(pretty.as_ref()),
-            desc: String::from(desc.as_ref()),
-        };
-        Node::Variable(Variable { info, data_type })
+        let info = NodeInfo::new(id, name, pretty, desc);
+        Node::Variable(Variable::new(info, data_type))
     }
 
     pub fn placeholder(id: NodeID) -> Self {
@@ -149,32 +139,19 @@ impl Node {
         &self.info().desc
     }
 
+    // TODO: what would this enable us to do? It would make lifetimes significantly more complicated
     // pub fn get_parent(&self) -> Option<Weak<Node>> {
     //     self.parent.clone()
     // }
 
-    // fn topic(&self) -> &String;
-    // fn path(&self) -> Vec<String>;
+    // TODO: do we need this? or does Rust's ownership handle this? Why do we need to know the node's owner?
+    // virtual void set_owner(const std::weak_ptr<context>& c) {
+    //     if (owner_.lock() && c.lock())
+    //         throw tree_error("node already has owner!");
+    //     owner_ = c;
+    // }
 
-    // fn from_path(&self, p: &Vec<&str>, idx: usize) -> Weak<dyn Node>;
-
-    // pub fn children(&self) -> Vec<Weak<Node>> {
-    pub fn children(&self) -> Option<&Vec<Node>> {
-        if let Node::Group(group) = self {
-            Some(&group.children)
-        } else {
-            None
-        }
-    }
-
-    // TODO: figure out what this is for
-    //         virtual void set_owner(const std::weak_ptr<context>& c) {
-    //             if (owner_.lock() && c.lock())
-    //                 throw tree_error("node already has owner!");
-    //             owner_ = c;
-    //         }
-
-    //         virtual void set_unowned() { owner_.reset(); }
+    // virtual void set_unowned() { owner_.reset(); }
 
     /// Deserialize this node from a protobuf node
     pub fn unpack(proto: &wire::Node) -> Result<Self, UnpackError> {
@@ -209,20 +186,6 @@ impl Node {
             Node::Variable(_) => "Variable",
         }
     }
-
-    //         virtual std::unique_ptr<node> clone() const = 0;
-    //     protected:
-    //         constexpr void set_parent(group* g) { parent_ = g; }
-    //         virtual void print(std::ostream& o, int ident=0) const;
-
-    //         id id_;
-    //         std::string name_;
-    //         std::string pretty_; // For display
-    //         std::string desc_; // For documentation
-
-    //         group* parent_;
-    //         std::weak_ptr<context> owner_;
-    //     }
 }
 
 impl fmt::Display for Node {
@@ -247,28 +210,50 @@ impl fmt::Display for Node {
 /// A group of nodes (an internal vertex of the node tree)
 pub struct Group {
     info: NodeInfo,
-    // TODO: should this be a vector? And then we store an auxiliary hashmap? That's the way that
-    // the C++ does it, but I don't see any advantage to doing it that way
-    // pub children: HashMap<String, Rc<Node>>,
-    // TODO: for now I am making this a vector, but I am still not convinced which it should be
-    pub children: Vec<Node>,
-
-    pub schema: String,
-    pub version: i32,
+    children: HashMap<String, Node>,
+    schema: String,
+    version: i32,
 }
 
 impl Group {
     pub fn new(info: NodeInfo, children: Vec<Node>, schema: String, version: i32) -> Self {
         Self {
             info,
-            children,
+            children: children
+                .into_iter()
+                .map(|node| (node.name().clone(), node))
+                .collect(),
             schema,
             version,
         }
     }
 
+    pub fn children_iter(&self) -> impl Iterator<Item = &Node> {
+        self.children.values()
+    }
+
+    pub fn children_iter_mut(&mut self) -> impl Iterator<Item = &mut Node> {
+        self.children.values_mut()
+    }
+
+    pub fn get_child<S: AsRef<str>>(&self, idx: S) -> Option<&Node> {
+        self.children.get(&idx.as_ref().to_string())
+    }
+
+    pub fn get_child_mut<S: AsRef<str>>(&mut self, idx: S) -> Option<&mut Node> {
+        self.children.get_mut(&idx.as_ref().to_string())
+    }
+
+    pub fn schema(&self) -> &String {
+        &self.schema
+    }
+
+    pub fn version(&self) -> i32 {
+        self.version
+    }
+
     pub fn pack_children(&self) -> Vec<wire::Node> {
-        unimplemented!()
+        self.children_iter().map(|n| n.pack()).collect()
     }
 
     /// Serialize this node back into a protobuf node
@@ -299,192 +284,71 @@ impl Group {
                 .children
                 .iter()
                 .flat_map(|node| Node::unpack(node))
+                .map(|node| (node.name().clone(), node))
                 .collect(),
             schema: group.schema.clone(),
             version: group.version,
         })
     }
+
+    // void set_owner(const std::weak_ptr<context>& c) override {
+    //     node::set_owner(c);
+    //     for (node* child : children_) child->set_owner(c);
+    // }
+
+    // void set_unowned() override {
+    //     node::set_unowned();
+    //     for (node* c : children_) c->set_unowned();
+    // }
+
+    // node* from_path(const std::vector<std::string_view>& p,
+    //     size_t idx=0) override {
+    //     if (idx > p.size()) return nullptr;
+    //     else if (idx == p.size()) return this;
+    //     else {
+    //         auto it = children_map_.find(p[idx]);
+    //         if (it == children_map_.end()) return nullptr;
+    //         return it->second->from_path(p, ++idx);
+    //     }
+    // }
+    // const node* from_path(const std::vector<std::string_view>& p,
+    //     size_t idx=0) const override {
+    //     if (idx > p.size()) return nullptr;
+    //     else if (idx == p.size()) return this;
+    //     else {
+    //         auto it = children_map_.find(p[idx]);
+    //         if (it == children_map_.end()) return nullptr;
+    //         return it->second->from_path(p, ++idx);
+    //     }
+    // }
+
+    // template<typename M>
+    //     bool resolve_placeholders(M* nodes) {
+    //         for (node::id p : placeholders_) {
+    //             if (nodes->find(p) == nodes->end()) return false;
+    //         }
+    //         for (node::id p : placeholders_) {
+    //             auto nodes_it = nodes->find(p);
+    //             if (nodes_it == nodes->end()) return false;
+    //             node* child = nodes_it->second;
+
+    //             children_.push_back(child);
+    //             child->set_parent(this);
+    //             children_map_[child->get_name()] = child;
+
+    //             nodes->erase(nodes_it);
+    //         }
+    //         return true;
+    //     }
 }
 
-// TODO: figure out how to make indexing work. Do we want to be able to index by ints?
-// impl Index<usize> for Node {
-//     type Output = Option<Weak<Node>>;
+impl<S: AsRef<str>> Index<S> for Group {
+    type Output = Node;
 
-//     fn index(&self, idx: usize) -> &Self::Output {
-//         if let NodeType::Group(group) = &self.node_type {
-
-//         } else {
-//             &None
-//         }
-//     }
-// }
-
-// class group : public node {
-// public:
-//     group(id i, const std::string_view& name, const std::string_view& pretty,
-//                 const std::string_view& desc, const std::string_view& schema, int version,
-//                 std::vector<node*>&& children) :
-//             node(i, name, pretty, desc),
-//             schema_(schema), version_(version), placeholders_(),
-//             children_(children), children_map_() {
-//         // populate the children map
-//         for (node* n : children_) {
-//             n->set_parent(this);
-//             children_map_[n->get_name()] = n;
-//         }
-//     }
-//     group(id i, const std::string_view& name, const std::string_view& pretty,
-//                 const std::string_view& desc, const std::string_view& schema, int version,
-//                 std::vector<node::id>&& placeholders) :
-//             node(i, name, pretty, desc),
-//             schema_(schema), version_(version), placeholders_(std::move(placeholders)),
-//             children_(), children_map_() {
-//         // populate the children map
-//         for (node* n : children_) {
-//             n->set_parent(this);
-//             children_map_[n->get_name()] = n;
-//         }
-//     }
-//     group(const group& g) :
-//         node(g), schema_(g.get_schema()),
-//         version_(g.get_version()), children_(),
-//         children_map_() {
-//         for (const node* n : g) {
-//             std::unique_ptr<node> nn = n->clone();
-//             node* r = nn.release();
-//             r->set_parent(this);
-//             children_.push_back(r);
-//             children_map_[r->get_name()] = r;
-//         }
-//     }
-//     ~group() {
-//         for (node* n : children_) delete n;
-//     }
-
-//     const std::string& get_schema() const { return schema_; }
-//     int get_version() const { return version_; }
-
-//     void set_owner(const std::weak_ptr<context>& c) override {
-//         node::set_owner(c);
-//         for (node* child : children_) child->set_owner(c);
-//     }
-
-//     void set_unowned() override {
-//         node::set_unowned();
-//         for (node* c : children_) c->set_unowned();
-//     }
-
-//     node* from_path(const std::vector<std::string_view>& p,
-//                             size_t idx=0) override {
-//         if (idx > p.size()) return nullptr;
-//         else if (idx == p.size()) return this;
-//         else {
-//             auto it = children_map_.find(p[idx]);
-//             if (it == children_map_.end()) return nullptr;
-//             return it->second->from_path(p, ++idx);
-//         }
-//     }
-//     const node* from_path(const std::vector<std::string_view>& p,
-//                                     size_t idx=0) const override {
-//         if (idx > p.size()) return nullptr;
-//         else if (idx == p.size()) return this;
-//         else {
-//             auto it = children_map_.find(p[idx]);
-//             if (it == children_map_.end()) return nullptr;
-//             return it->second->from_path(p, ++idx);
-//         }
-//     }
-
-//     std::vector<node*> nodes() override {
-//         std::vector<node*> n;
-//         n.push_back(this);
-//         for (node* c : children_) {
-//             for (node* d : c->nodes()) {
-//                 n.push_back(d);
-//             }
-//         }
-//         return n;
-//     }
-//     std::vector<const node*> nodes() const override {
-//         std::vector<const node*> n;
-//         n.push_back(this);
-//         for (const node* c : children_) {
-//             for (const node* d : c->nodes()) {
-//                 n.push_back(d);
-//             }
-//         }
-//         return n;
-//     }
-
-//     const std::vector<node::id>& placeholders() const {
-//         return placeholders_;
-//     }
-
-//     node* operator[](size_t idx) override {
-//         if (idx >= children_.size()) return nullptr;
-//         return children_[idx];
-//     }
-//     const node* operator[](size_t idx) const override {
-//         if (idx >= children_.size()) return nullptr;
-//         return children_[idx];
-//     }
-//     node* operator[](const std::string& child) override {
-//         try {
-//             return children_map_.at(child);
-//         } catch (const std::out_of_range& e) {
-//             return nullptr;
-//         }
-//     }
-//     const node* operator[](const std::string& child) const override {
-//         try {
-//             return children_map_.at(child);
-//         } catch (const std::out_of_range& e) {
-//             return nullptr;
-//         }
-//     }
-//     inline std::vector<node*>::iterator begin() { return children_.begin(); }
-//     inline std::vector<node*>::const_iterator begin() const { return children_.begin(); }
-//     inline std::vector<node*>::iterator end() { return children_.end(); }
-//     inline std::vector<node*>::const_iterator end() const { return children_.end(); }
-
-//     inline size_t num_children() const { return children_.size(); }
-
-//     void pack(Group* group) const;
-//     virtual void pack(Node* proto) const override;
-//     static group* unpack(const Group& g);
-
-//     template<typename M>
-//         bool resolve_placeholders(M* nodes) {
-//             for (node::id p : placeholders_) {
-//                 if (nodes->find(p) == nodes->end()) return false;
-//             }
-//             for (node::id p : placeholders_) {
-//                 auto nodes_it = nodes->find(p);
-//                 if (nodes_it == nodes->end()) return false;
-//                 node* child = nodes_it->second;
-
-//                 children_.push_back(child);
-//                 child->set_parent(this);
-//                 children_map_[child->get_name()] = child;
-
-//                 nodes->erase(nodes_it);
-//             }
-//             return true;
-//         }
-
-//     std::unique_ptr<node> clone() const override {
-//         return std::make_unique<group>(*this);
-//     }
-// private:
-//     void print(std::ostream& o, int ident=0) const override;
-
-//     std::string schema_;
-//     int version_;
-
-//     std::vector<node::id> placeholders_;
-//     std::vector<node*> children_;
-//     std::map<std::string, node*, std::less<>> children_map_;
-// };
+    fn index(&self, idx: S) -> &Self::Output {
+        &self.children[&idx.as_ref().to_string()]
+    }
+}
 
 /*
  * Actions
@@ -545,33 +409,15 @@ impl Action {
             },
         })
     }
+
+    pub fn get_arg_type(&self) -> &Type {
+        &self.arg_type
+    }
+
+    pub fn get_ret_type(&self) -> &Type {
+        &self.ret_type
+    }
 }
-
-// class action : public node {
-// public:
-//     action(id i, const std::string_view& name,
-//             const std::string_view& pretty, const std::string_view& desc,
-//             const value_type& arg_type, const value_type& ret_type) :
-//             node(i, name, pretty, desc),
-//             arg_type_(arg_type), ret_type_(ret_type) {}
-//     action(const action& a) : node(a),
-//         arg_type_(a.get_arg_type()),
-//         ret_type_(a.get_ret_type()) {}
-//     const value_type& get_arg_type() const { return arg_type_; }
-//     const value_type& get_ret_type() const { return ret_type_; }
-
-//     void pack(Action* proto) const;
-//     virtual void pack(Node* proto) const override;
-//     static action* unpack(const Action& proto);
-
-//     std::unique_ptr<node> clone() const override {
-//         return std::make_unique<action>(*this);
-//     }
-// private:
-//     void print(std::ostream& o, int ident=0) const override;
-//     value_type arg_type_;
-//     value_type ret_type_;
-// };
 
 /*
  * Variables
@@ -614,30 +460,13 @@ impl Variable {
             },
         })
     }
+
+    pub fn get_type(&self) -> &Type {
+        &self.data_type
+    }
+
+    // bool compatible_with(node* other) const override;
 }
-
-// class variable : public node {
-// public:
-//     variable(id i, const std::string_view& name,
-//             const std::string_view& pretty, const std::string_view& desc,
-//             const value_type& t) : node(i, name, pretty, desc), data_type_(t) {}
-//     variable(const variable& v) :
-//         node(v), data_type_(v.get_type()) {}
-//     const value_type& get_type() const { return data_type_; }
-
-//     bool compatible_with(node* other) const override;
-
-//     void pack(Variable* var) const;
-//     virtual void pack(Node* proto) const override;
-//     static variable* unpack(const Variable& proto);
-
-//     std::unique_ptr<node> clone() const override {
-//         return std::make_unique<variable>(*this);
-//     }
-// private:
-//     void print(std::ostream& o, int ident=0) const override;
-//     value_type data_type_;
-// };
 
 /*
  * Placeholder
